bd582fc2ea81aee317ad2684497791a1cab0d13e
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-16 21:53:05 +0530

    Add files via upload

---
+++ public class Pyramid.java
@@ -0,0 +1,15 @@
+public class Pyramid
+{
+    public static void main(Srting[args])
+    {
+        int n=4;
+        for (int i=0;i<n;i++)
+        {
+            System.out.println("\n");
+            for(int j=0;j<=i;j++)
+            {
+                System.out.println("*");
+            }
+        }
+    }
+} 
